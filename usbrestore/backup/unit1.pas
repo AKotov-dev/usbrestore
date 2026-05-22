19d2265a3a27a47c5e21e3fb7c88cccf4135319b
AKotov-dev <akotov830@gmail.com>
2026-05-22 16:50:13 +0300

    v1.1 + Added support for exFAT file system

--- usbrestore/backup/unit1.pas
+++ usbrestore/backup/unit1.pas
@@ -136,8 +136,21 @@ begin
 end;
 
 procedure TMainForm.FormShow(Sender: TObject);
+var
+  bmp: TBitmap;
 begin
   IniPropStorage1.Restore;
+
+  // Устраняем баг иконки приложения
+  bmp := TBitmap.Create;
+  try
+    bmp.PixelFormat := pf32bit;
+    bmp.Assign(Image1.Picture.Graphic);
+    Application.Icon.Assign(bmp);
+  finally
+    bmp.Free;
+  end;
+
   MainForm.Caption := Application.Title;
   ReloadBtn.Width := ReloadBtn.Height;
 
